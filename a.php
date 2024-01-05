d0c9383a4573328b0a97405c77e2f6539295eda1
koonpollzaza <152575123+koonpollzaza@users.noreply.github.com>
2024-01-05 08:49:05 +0700

    Ex1 php1
    Ex1 php1

---
+++ a.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $a = 4;
+    $b = 10;
+    $c = ($b - 5) * $a;
+
+    echo "Result 1 = " , (. $c ."<BR>";
+    echo "Result 2 = " , (. $c ."<BR>";
+    echo "Result 3 = " , (. $c ."<BR>";
+    echo "Result 4 = " , ($c / $d) , "<BR>";
+    ?>
+</body>
+</html>
